fb2fdc6c905ac0b59d5da62bed7a2a7324dba56b
Ruslan Iliasov <Ruslanboss459@gmail.com>
2024-06-20 19:06:04 +0300

    Final project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+	const [todos, setTodos] = useState([]);
 
-export default App;
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((rawTodosData) => rawTodosData.json())
+			.then((todosData) => setTodos(todosData));
+	}, []);
+
+	return (
+		<div className={styles.App}>
+			{todos.map(({ id, title }) => (
+				<div className={styles.todo} key={id}>
+					{title}
+				</div>
+			))}
+		</div>
+	);
+};
